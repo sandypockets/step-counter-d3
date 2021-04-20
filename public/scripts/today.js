708306e082fce68199abd397ac29f5788ba92459
sandypockets <zac@znacreative.com>
2021-04-19 22:24:47 -0400

    Refactor bars to vertical

--- public/scripts/today.js
+++ public/scripts/today.js
@@ -1,7 +1,5 @@
 const todayData = [
-  100, 113, 300, 50, 100, 800, 300,
-  450, 530, 630, 42, 30, 80, 90, 100,
-  240, 30, 60, 40, 60
+  12, 110, 56, 85, 29, 123, 34, 56, 89, 12, 2, 67, 33, 99, 35, 13, 54, 54, 29, 10
 ];
 
 const todaySvg = d3.select('svg');
@@ -11,11 +9,13 @@ todaySvg
   .data(todayData)
   .enter()
   .append('rect')
-  .attr('width', (dataPoint, index) => {
+  .attr('width', 24)
+  .attr('y', (dataPoint, index) => {
+    return 112 - dataPoint;
+  })
+  .attr('x', (dataPoint, index) => {
+    return 36 * index;
+  })
+  .attr('height', (dataPoint, index) => {
     return dataPoint;
   })
-  .attr('y', (dataPoint, index) => {
-    return 40 * index;
-  })
-  .attr('x', 0)
-  .attr('height', 30)
