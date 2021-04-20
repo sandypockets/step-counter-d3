02cb7121edaab117d373bedb7ad58bd8c4f29c43
sandypockets <zac@znacreative.com>
2021-04-19 21:44:23 -0400

    Add new data values for testing

--- public/scripts/vanilla.js
+++ public/scripts/vanilla.js
@@ -1,12 +1,17 @@
-const data = [550, 420, 731, 420, 130, 670, 356, 243];
+const data = [10, 30, 50, 10, 80, 30, 70, 50, 60, 4, 30, 80, 90, 100, 240, 30, 60, 40, 60];
 
 const todaySvg = document.querySelector('svg');
 
 data.forEach((dataPoint, index) => {
   const rectTag = document.createElementNS(`http://www.w3.org/2000/svg`, `rect`);
-  rectTag.setAttribute('x', 0);
-  rectTag.setAttribute('y', index * 50);
-  rectTag.setAttribute('width', dataPoint);
-  rectTag.setAttribute('height', '40');
+  rectTag.setAttribute('x', index * 36);
+  rectTag.setAttribute('y', 112 - dataPoint);
+  rectTag.setAttribute('width', '24');
+  rectTag.setAttribute('height', dataPoint);
   todaySvg.append(rectTag);
 })
+
+// Convert bars to vertical
+// width - 24
+// space between bars - 12
+// max height - 112
