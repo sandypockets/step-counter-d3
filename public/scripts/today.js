7815f85081348a33b4bdb8fd6869b80008ec8e0e
sandypockets <zac@znacreative.com>
2021-04-19 22:20:23 -0400

    Use D3 to dynamically insert bar chart

--- public/scripts/today.js
+++ public/scripts/today.js
@@ -1,9 +1,21 @@
-const todayData = [10, 30, 5, 10, 80, 30, 70, 50, 60, 4, 30, 80, 90, 100, 240, 30, 60, 40, 60];
+const todayData = [
+  100, 113, 300, 50, 100, 800, 300,
+  450, 530, 630, 42, 30, 80, 90, 100,
+  240, 30, 60, 40, 60
+];
 
 const todaySvg = d3.select('svg');
-const rectTags = d3.selectAll('rect');
 
-
-rectTags
+todaySvg
+  .selectAll('rect')
   .data(todayData)
-  .attr('width', 600)
+  .enter()
+  .append('rect')
+  .attr('width', (dataPoint, index) => {
+    return dataPoint;
+  })
+  .attr('y', (dataPoint, index) => {
+    return 40 * index;
+  })
+  .attr('x', 0)
+  .attr('height', 30)
