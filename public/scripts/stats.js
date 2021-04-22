809b2f474180a7c579b3664889d86890c276ffac
sandypockets <zac@znacreative.com>
2021-04-22 01:11:56 -0400

    d3.format to add comma to stats numbers

--- public/scripts/stats.js
+++ public/scripts/stats.js
@@ -1,5 +1,7 @@
-d3.select('p.worst-day').text(d3.min(monthData) + ' steps') // Minimum val in monthData array
-d3.select('p.average-day').text(Math.floor(d3.mean(monthData)) + ' steps') // Avg of monthData
-d3.select('p.best-day').text(d3.max(monthData) + ' steps') // Max val in monthData
-d3.select('p.total-month').text(d3.sum(monthData) + ' steps') // Sum of monthData
-d3.select('p.total-day').text(d3.sum(todayData) + ' steps') // Sum of todayData
+const statsFormat = d3.format(',') // Adds a comma to the number where appropriate
+
+d3.select('p.worst-day').text(statsFormat(d3.min(monthData)) + ' steps') // Minimum val in monthData array
+d3.select('p.average-day').text(statsFormat(Math.floor(d3.mean(monthData))) + ' steps') // Avg of monthData
+d3.select('p.best-day').text(statsFormat(d3.max(monthData)) + ' steps') // Max val in monthData
+d3.select('p.total-month').text(statsFormat(d3.sum(monthData)) + ' steps') // Sum of monthData
+d3.select('p.total-day').text(statsFormat(d3.sum(todayData)) + ' steps') // Sum of todayData
